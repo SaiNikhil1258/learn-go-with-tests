cfe3b113947c2e0998681893e38e4003e8946725
Nikhil <sainikhil1258@gmail.com>
2024-07-16 15:34:09 +0530

    maps with tests

---
+++ maps/dictionary.go
@@ -0,0 +1,54 @@
+package maps
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("could not add word already exists")
+	ErrWordDoesNotExist = DictionaryErr("could not update word, it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	def, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return def, nil
+}
+
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = def
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = def
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	delete(d, word)
+	return nil
+}
